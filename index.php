3ee70effa3dfe807f84e7860776ba7139f2aae02
SarthakPhatate <57782357+SarthakPhatate@users.noreply.github.com>
2020-06-14 16:29:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 Auto Launching of Webserver using Terraform 
 <?php
-header('Content-type: image/jpeg');
-echo file_get_contents("https://hmc1-bucket.s3.ap-south-1.amazonaws.com/new_object_key");
+"Task done Successful"
 ?>
